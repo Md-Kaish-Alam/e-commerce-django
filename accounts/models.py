6e3e6a8721413a551616d530ffc27aef94f46b87
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-05-27 14:20:19 +0530

    added review & rating functionality

--- accounts/models.py
+++ accounts/models.py
@@ -68,6 +68,9 @@ class Account(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     # if the user is admin then it have all the permissions
     def has_perm(self, perm, obj=None):
         return self.is_admin
